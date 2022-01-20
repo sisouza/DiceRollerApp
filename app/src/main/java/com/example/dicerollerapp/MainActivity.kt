1f16e5135a580c43f423535fd489b9f553112a89
Sidia <sidia.reis1@gmail.com>
2022-01-19 20:24:12 -0800

    display button number

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.dicerollerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +13,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // sets a reference to the Button object in the variable
         val rollButton: Button = findViewById(R.id.btnRoll)
+        //function to show message
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.tvText)
+            resultTextView.text = "6"
+        }
 
     }
 }
