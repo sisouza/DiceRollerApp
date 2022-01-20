c82e53a5702e693faae730fedcffaa4cd19eec44
Sidia <sidia.reis1@gmail.com>
2022-01-19 20:13:20 -0800

    set button object reference

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.dicerollerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // sets a reference to the Button object in the variable
+        val rollButton: Button = findViewById(R.id.btnRoll)
+
     }
 }
